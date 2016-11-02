684ecf7d7ff097da6b1bdf9e95a96799466a0af2
Paul des Garets <paul.des-garets@student.ecp.fr>
2016-11-02 15:45:25 +0100

    fix and disable vbadd

--- scripts/hyperv.sh
+++ scripts/hyperv.sh
@@ -2,17 +2,17 @@
 echo 'deb http://http.debian.net/debian jessie-backports main' > /etc/apt/sources.list.d/backports.list
 
 apt-get update
-ker=$(/bin/uname -r)
-apt-get install -t jessie-backports -y linux-image-${ker} hyperv-daemons
+kernel=$(/usr/bin/apt-cache search linux-image|/bin/grep -v dbg|/bin/grep -v unsigned|/bin/grep -v grsec|/usr/bin/tail -n1|/usr/bin/cut -d' ' -f1)
+apt-get install -t jessie-backports -y ${kernel} hyperv-daemons
 
-mods=( hv_vmbus hv_storvsc hv_blkvsc hv_netvsc )
-module_file=/etc/initramfs-tools/modules
-for mod in ${mods[@]}
-do
-    if ! grep $mod $module_file >> /dev/null; then
-        echo  "${mod}" >> $module_file
-    fi
-done
+# mods=( hv_vmbus hv_storvsc hv_blkvsc hv_netvsc )
+# module_file=/etc/initramfs-tools/modules
+# for mod in ${mods[@]}
+# do
+#     if ! grep $mod $module_file >> /dev/null; then
+#         echo  "${mod}" >> $module_file
+#     fi
+# done
 
-echo "Created $(cat ${module_file})"
+# echo "Created $(cat ${module_file})"
 eval /usr/sbin/update-initramfs -u
